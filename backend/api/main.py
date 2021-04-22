5a959a78f556dd50d3260b38ecf16ec7c87e55d0
Daniel <danilmaleev@gmail.com>
2021-04-22 15:36:22 -0500

    sockets first

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from fastapi import Depends, FastAPI, HTTPException, status, WebSocket, Cookie, Query
+from fastapi import Depends, FastAPI, HTTPException, status, WebSocket, Cookie, Query, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from starlette.middleware.cors import CORSMiddleware
 
@@ -9,6 +9,8 @@ from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
+
+import json
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -66,45 +68,7 @@ def get_thread(thread_id: int, db: Session = Depends(get_db)):
     messages = crud.get_messages(db=db, thread=thread)
     return {'messages': messages}
     
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
+    
 class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
@@ -119,17 +83,12 @@ class ConnectionManager:
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
-    async def broadcast(self, message: str):
+    async def broadcast(self, message: str, username: str):
         for connection in self.active_connections:
-            await connection.send_text(message)
+            await connection.send_text({"message":message, "username": username})
 
 
 manager = ConnectionManager()
-
-
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
 
 
 @app.websocket("/ws/{client_id}")
@@ -138,8 +97,7 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            print(data)
+            await manager.broadcast(json.loads(data)["message"], json.loads(data)["username"])
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
